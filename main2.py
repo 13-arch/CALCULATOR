e0368d7ca57f7bf8d7ab23a8c053bcf3b862e1f7
13-arch <Iron_Mike@tutamail.com>
2024-12-02 13:30:59 +0300

    kal#2

--- main2.py
+++ main2.py
@@ -6,28 +6,55 @@ def multiply(a,b):
     return a*b
 def divide(a,b):
     return a/b
+def factorial(a):
+    factorial=1
+    for i in range(2, a+1):
+        factorial *= i
+    return factorial
+def degree(a,b):
+    return a**b
+
+
+
+
 vir=input('введите выражение: ')
 if "+" in vir:
-    vir= vir.split('+')
-    for i in range(0,len(vir)):
-        vir[i]=float(vir[i])
-    result=addition(vir[0],vir[i])
+    v= vir.split('+')
+    result = float(v[0])
+    for i in range(1,len(v)):
+        v[i]=float(v[i])        
+        result=addition(result,v[i])
     print(result)
-if "-" in vir:
-    vir= vir.split('-')
-    for i in range(0,len(vir)):
-        vir[i]=float(vir[i])
-    result=subtract(vir[0],vir[i])
+elif "-" in vir:
+    v= vir.split('-')
+    result = float(v[0])
+    for i in range(1,len(v)):
+        v[i]=float(v[i])
+        result=subtract(result,v[i])
     print(result)
-if "*" in vir:
-    vir= vir.split('*')
-    for i in range(0,len(vir)):
-        vir[i]=float(vir[i])
-    result=multiply(vir[0],vir[i])
+elif "*" in vir:
+    v= vir.split('*')
+    result = float(v[0])
+    for i in range(1,len(v)):
+        v[i]=float(v[i])
+        result=multiply(result,v[i])
     print(result)
-if "/" in vir:
-    vir= vir.split('/')
-    for i in range(0,len(vir)):
-        vir[i]=float(vir[i])
-    result=divide(vir[0],vir[i])
+elif "/" in vir:
+    v= vir.split('/')
+    result = float(v[0])
+    for i in range(1,len(v)):
+        v[i]=float(v[i])
+        result=divide(result,v[i])
+    print(result)
+elif "!" in vir:
+    v= vir.split('!')
+    v=v[0].split(' ')
+    result=factorial(int(v[-1]))
+    print(result)
+elif "^" in vir:
+    v=vir.split('^')
+    result = float(v[0])
+    for i in range(1,len(v)):
+        v[i]=float(v[i])
+        result=degree(result,v[i])
     print(result)
